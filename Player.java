da4989c66f4e4b7a5825aa436f6509c8dfec121a
nbezanis <yackamajez@gmail.com>
2019-01-23 22:41:18 -0500

    added x and y to the player constructor

--- Player.java
+++ Player.java
@@ -8,8 +8,9 @@ public class Player {
 	public final int WIDTH = 50;
 	public final int HEIGHT = 100;//width and height of the stick things, 50 and 100 are placeholders just change it to whatever looks the best
 	
-	public Player() {//just a default constructor, I don't really have anything to put in here for now but maybe something will come up later
-		
+	public Player(int x, int y) {//just a default constructor, I don't really have anything to put in here for now but maybe something will come up later
+		this.x = x;//the x and y in the constructor are for the sticks' starting positions
+		this.y = y;
 	}
 	
 	//this function is for updating the player's position data and stuff
